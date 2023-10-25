ff2733931747927a753e21bbbb592114577ca224
Divyanshu <kumardivyansh124@gmail.com>
2023-10-25 22:06:41 +0530

    25oct

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ const addEventOnElements = function(elements, eventType, callback){
     }
 }
 
-const navbar= document.querySelector("[data-nabvbar]");
+const navbar= document.querySelector("[data-navbar]");
 const navTogglers= document.querySelectorAll("[data-nav-toggler]");
 const overlay=document.querySelector("[data-overlay]");
 
 const toggleNav=function(){
-    navbar.classlist.toggle("active");
+    navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
 }
 
 addEventOnElements(navTogglers, "click", toggleNav);
+
+/**
+ * Header
+ */
+const header= document.querySelector("[data-header]");
+window.addEventListener("scroll", function(){
+    header,classList[window.scrollY>100 ? "add" : "remove"]("active");
+});
