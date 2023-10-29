6fbdf80d6efd7df265e52cb822cc94c2960ad711
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-10-29 23:57:43 +0530

    text fix

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const preloader = document.querySelector("[data-preloader]");
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     preloader.classList.add("remove");
 });
 
-const addEventOnElements = function(elements, eventType, callback){
-    for (let i=0, len = elements.length; i < len; i++){
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
         elements[i].addEventListener(eventType, callback);
     }
 }
 
-const navbar= document.querySelector("[data-navbar]");
-const navTogglers= document.querySelectorAll("[data-nav-toggler]");
-const overlay=document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
 
-const toggleNav=function(){
+const toggleNav = function () {
     navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
@@ -24,7 +24,7 @@ addEventOnElements(navTogglers, "click", toggleNav);
 /**
  * Header
  */
-const header= document.querySelector("[data-header]");
-window.addEventListener("scroll", function(){
-    header,classList[window.scrollY>100 ? "add" : "remove"]("active");
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", function () {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
